e181363163894e9d028bbf56d95790c574f05056
“Esteban-Somoza” <estfersomoza@gmail.com>
2022-08-17 17:37:50 -0300

    etapa 2 - mantenimientos

--- ejercicio.js
+++ ejercicio.js
@@ -1,3 +1,8 @@
+// requires
+let Moment = require('moment')
+let MomentRange = require('moment-range')
+const moment = MomentRange.extendMoment(Moment);
+
 // Definicion de vehiculos
 
 let camioneta = {
@@ -12,7 +17,8 @@ let camioneta = {
         let consumoCombustible = kilometrajeTotal * 0.1
         return consumoCombustible
     },
-    viajeAsignado: null
+    viajeAsignado: null,
+    mantenimiento: []
 }
 
 let combi = {
@@ -46,7 +52,8 @@ let combi = {
         let consumoCombustible = kilometrajeTotal * consumo
         return consumoCombustible
     },
-    viajeAsignado: null
+    viajeAsignado: null,
+    mantenimiento: []
 }
 
 let camioncito = {
@@ -72,7 +79,8 @@ let camioncito = {
         let consumoCombustible = 1 * kmBuenEstado * consumoPorCarga + 1.5 * kmMalEstado * consumoPorCarga
         return consumoCombustible
     },
-    viajeAsignado: null
+    viajeAsignado: null,
+    mantenimiento: []
 }
 
 let camionazo = {
@@ -96,7 +104,8 @@ let camionazo = {
         let consumoCombustible = 1 * kilometrajeTotal * consumoPorCarga + 0.3 * kilometrajeTotal
         return consumoCombustible
     },
-    viajeAsignado: null
+    viajeAsignado: null,
+    mantenimiento: []
 }
 
 // bases de datos
@@ -110,32 +119,46 @@ let baseDeDatosLugares = ["lugarA", "lugarB", "lugarC", "lugarD"]
 let operaciones = {
     vehiculosDisponibles: baseDeDatosVehiculos,
 
+    seleccionarVehiculo: function (vehiculo) {
+        return this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
+    },
+
     ruta: function (lugarA, lugarB) {
         let lugares = baseDeDatosLugares
         // iria una especie de grafo con nodos
-        return { kmBuenEstado: 150, kmMalEstado: 2000 }
+        return { kmBuenEstado: 150, kmMalEstado: 500 }
     },
 
-    vehiculosConCapacidadDeViaje: function (trayecto, carga, tiempoMaximo) {
+    vehiculosConCapacidadDeViaje: function (trayecto, carga, duracionMaximaViaje) {
         let ruta = this.ruta(trayecto.puntoA, trayecto.puntoB)
+        let comienzoViaje = new Date()
+        let finalizacionViaje = new Date(comienzoViaje.getTime() + duracionMaximaViaje * 60 * 60 * 1000);
         this.vehiculosDisponibles = this.vehiculosDisponibles.filter(v => v.cargaMaxima >= carga)
-        this.vehiculosDisponibles = this.vehiculosDisponibles.filter(v => v.tiempoParaViaje(ruta.kmBuenEstado, ruta.kmMalEstado, carga) <= tiempoMaximo)
-
+        this.vehiculosDisponibles = this.vehiculosDisponibles.filter(v => v.tiempoParaViaje(ruta.kmBuenEstado, ruta.kmMalEstado, carga) <= duracionMaximaViaje)
+        this.vehiculosDisponibles = this.vehiculosDisponibles.filter(v => this.consultaMantenimiento(v.tipo, comienzoViaje, finalizacionViaje))
         return this.vehiculosDisponibles
     },
 
-
     asignarViaje: function (vehiculo, viaje) {
-        let vehiculoAAsignar = this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
-        if (!vehiculoAAsignar) return console.log("No hay vehicuos disponibles")
-        return vehiculoAAsignar.viajeAsignado = viaje
+        // let vehiculoAAsignar = this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
+        let vehiculoSeleccionado = this.seleccionarVehiculo(vehiculo)
+        if (!vehiculoSeleccionado) return console.log("No hay vehicuos disponibles")
+        return vehiculoSeleccionado.viajeAsignado = viaje
+    },
+
+    desasignarViaje: function (vehiculo, viaje) {
+        // let vehiculoAAsignar = this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
+        let vehiculoSeleccionado = this.seleccionarVehiculo(vehiculo)
+        // let quitarViaje = vehiculoSeleccionado.viajeAsignado.filter(v => v != viaje)
+        return vehiculoSeleccionado.viajeAsignado = {}
     },
 
     calcularConsumo: function (vehiculo) {
-        let vehiculoACalcular = this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
-        if (!vehiculoACalcular.viajeAsignado) return console.log("no habia viaje")
-        let ruta = this.ruta(vehiculoACalcular.puntoA, vehiculoACalcular.puntoB)
-        return vehiculoACalcular.consumoCombustible(ruta.kmBuenEstado, ruta.kmMalEstado, vehiculoACalcular.viajeAsignado.carga)
+        // let vehiculoACalcular = this.vehiculosDisponibles.find(v => v.tipo === vehiculo)
+        let vehiculoSeleccionado = this.seleccionarVehiculo(vehiculo)
+        if (!vehiculoSeleccionado.viajeAsignado) return console.log("no habia viaje")
+        let ruta = this.ruta(vehiculoSeleccionado.puntoA, vehiculoSeleccionado.puntoB)
+        return vehiculoSeleccionado.consumoCombustible(ruta.kmBuenEstado, ruta.kmMalEstado, vehiculoSeleccionado.viajeAsignado.carga)
     },
 
     calcularConsumoTotal: function () {
@@ -146,14 +169,56 @@ let operaciones = {
             }
         })
         return consumoTotal
+    },
+
+    asignarMantenimiento: function (vehiculo, entrada, duracion) {
+        let vehiculoSeleccionado = this.seleccionarVehiculo(vehiculo)
+        let salida = new Date(entrada.getTime() + duracion * 60 * 60 * 1000);
+        let mantenimiento = { entrada: entrada, salida: salida }
+        // console.log(entrada);
+        // console.log(salida);
+        if (this.consultaMantenimiento(vehiculoSeleccionado.tipo, entrada, salida) == false) {
+            return console.log("Fecha mantenimiento solapada");
+        }
+        return vehiculoSeleccionado.mantenimiento.push(mantenimiento)
+    },
+
+    consultaMantenimiento: function (vehiculo, fechaEntrada, fechaSalida) {
+        let fechasMantenimiento = this.seleccionarVehiculo(vehiculo).mantenimiento
+        let rangoActual = moment.range(fechaEntrada, fechaSalida)
+        if (fechasMantenimiento.length === 0) {
+            return true
+        }
+        for (let i = 0; i < fechasMantenimiento.length; i++) {
+            let rangoMantenimiento = moment.range(fechasMantenimiento[i].entrada, fechasMantenimiento[i].salida)
+            if (rangoMantenimiento.overlaps(rangoActual)) {
+                return false
+            }
+            else {
+                // console.log("ok");
+                return true
+            }
+        }
     }
+
 }
 
 
-// console.log(operaciones.vehiculosDisponibles)
-// operaciones.vehiculosConCapacidadDeViaje({ puntoA: "lugarA", puntoB: "lugarB" }, 2000, 18)
-// console.log(operaciones.vehiculosDisponibles)
 // operaciones.asignarViaje("camionazo", { puntoA: "lugarA", puntoB: "lugarB", carga: 2000 })
-// operaciones.asignarViaje("combi", { puntoA: "lugarA", puntoB: "lugarB", carga: 2000 })
+operaciones.asignarViaje("combi", { puntoA: "lugarA", puntoB: "lugarB", carga: 2000 })
+operaciones.desasignarViaje("combi", { puntoA: "lugarA", puntoB: "lugarB", carga: 2000 })
 // console.log(operaciones.calcularConsumo("camionazo"))
 // console.log(operaciones.calcularConsumoTotal())
+// let salida = new Date(entrada + "40:00:00")
+// console.log(salida);
+// console.log(operaciones.asignarMantenimiento("combi", entrada2, 70))
+// console.log(operaciones.seleccionarVehiculo("combi"))
+// operaciones.vehiculosConCapacidadDeViaje({ puntoA: "lugarA", puntoB: "lugarB" }, 600, 12)
+// console.log(operaciones.vehiculosConCapacidadDeViaje.forEach(date => !(date.fechaEntrada <= finalizacionViaje) && (comienzoViaje <= date.fechaSalida)))
+// operaciones.asignarMantenimiento("combi", entrada, 30)
+// operaciones.asignarMantenimiento("combi", entrada2, 30)
+// let entrada = new Date("August 19, 2022 08:30:00 UTC")
+// let entrada2 = new Date("August 22, 2022 08:30:00 UTC")
+console.log(operaciones.seleccionarVehiculo("combi"))
+
+
